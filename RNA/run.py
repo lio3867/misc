9c071928740de941fc622fb89f05e9ab4697f9d7
{lioc} <{lionel.chiron@gmail.com}>
2020-10-17 14:59:47 +0200

    rna

---
+++ RNA/run.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+
+"""
+
+##
+from flask import Flask, render_template, request, redirect    # Flask imports
+
+app = Flask(__name__)
+
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'F34TF$($e34D';
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    '''
+
+    '''
+    if request.method == 'POST':
+        data = request.form['seq']
+        print(data)
+        d = data.split(',')
+        try:
+            sentence, sep = d[0], d[1]
+            print(f'separator is {sep}')
+        except:
+            sentence = d[0]
+            sep = ''
+        dicseq = {'sentence':sentence, 'sep':sep}
+
+        return render_template('index.html',**dicseq) #, {'sentence':'ATGGCCA'}
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = 5033; host = '127.0.0.1'
+    app.run(port = port, host = host)
